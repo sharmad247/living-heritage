f3ede6211e994a687b5ab4c7919763cc10daa5d9
Dave Paiva <34977572+davepaiva@users.noreply.github.com>
2019-04-02 19:45:28 +0530

    changed geoquerying library to GeoFirestore.js

--- firebase-functions/index.js
+++ firebase-functions/index.js
@@ -1,110 +1,123 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true})
 const admin = require('firebase-admin')
-var geofirex = require('geofirex');
+const geofirestore = require('geofirestore');
 
-// Init firebase-admin SDK with acc credentials
+
 var serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const geo = geofirex.init(admin); //Init geofirex with firebase project
 
-// Firebase cloud function with URL: <project url>/postData
-//POST request function to post all tree-data
-// x-www-form-urlencoded method
-exports.postData = functions.https.onRequest((req, res)=>{   
- cors(req, res, ()=>{ // Cross-origin-Resource-Sharinf=g
+const defaultFirestore = admin.firestore();
+
+// Create a Firestore reference for Geofirestore 
+const  geoFirestore = new geofirestore.GeoFirestore(defaultFirestore);
+// Create a Collection reference
+const geoCollection = geoFirestore.collection('trees');
+
+
+
+
+
+
+
+exports.postData = functions.https.onRequest((req, res)=>{
+ cors(req, res, ()=>{
 
 
 
 
     if(req.method === 'POST'){
 
-        var docID = req.body.lat + '_' + req.body.long;  //Tree ID = lat_long
-        const trees = geo.collection('trees');  //Collection ID = 'trees'
+        var lat = Number(req.body.lat);
+        var long = Number(req.body.long);
     
-        const point = geo.point(Number(req.body.lat), Number(req.body.long));   //Convert lat long to geohash
-    
-        trees.setDoc(docID, { 
-            genericName: req.body.generic_name,
-            scientificName: req.body.scientific_name,
-            type: req.body.type,
-            leafStructure: req.body.leaf_structure,
-            rootStructure: req.body.root_structure,
-            age: req.body.age,
-            flower: req.body.flower,
-            fruit: req.body.fruit,
-            position: point.data,
+        geoCollection.add( { 
             
-            // Environmental Checkups acc to google doc 
-            cutBranches: req.body.cut_branches,
-            sap: req.body.sap,
-            branchCrack: req.body.cracked_branches,
-            brownmud: req.body.brownmud,
-            tumors: req.body.tumors,
-            fungus: req.body.fungus,
-            wilting: req.body.wilting,
-            saprophyte: req.body.saprophyte,
-            fire: req.body.fire,
-            stripped: req.body.stripped,
-            construction: req.body.construction,
-            branchesCut: req.body.brachesCut,
-            overgrownBranches: req.body.overgrownBranches,
-            cutTrees: req.body.cutTrees,
-            landBurn: req.body.landBurn,
-            highway: req.body.highway,
-            industrial: req.body.industrial,
-            publicLand: req.body.publicLand,
-            widened: req.body.widened,
-            habitatedPrivate: req.body.habitatedPrivate,
-            unhabitatedPrivate: req.body.unhabitatedPrivate,
-            centerProperty: req.body.centerProperty,
-            perimeterProperty: req.body.perimeterProperty,
-            forest: req.body.forest,
-        });
+            info : {
+                genericName: req.body.generic_name,
+                scientificName: req.body.scientific_name,
+                type: req.body.type,
+                leafStructure: req.body.leaf_structure,
+                rootStructure: req.body.root_structure,
+                age: req.body.age,
+                flower: req.body.flower,
+                fruit: req.body.fruit
+                    },
+            healthChecks: {
+                cutBranches: req.body.cut_branches,
+                sap: req.body.sap,
+                branchCrack: req.body.cracked_branches,
+                brownmud: req.body.brownmud,
+                tumours: req.body.tumours,
+                fungus: req.body.fungus,
+                wilting: req.body.wilting,
+                saprophyte: req.body.saprophyte,
+                fire: req.body.fire,
+                stripped: req.body.stripped,
+                construction: req.body.construction,
+                branchesCut: req.body.branchesCut,
+             },
+            environmentalRisks: {
+                overgrownBranches: req.body.overgrownBranches,
+                cutTrees: req.body.cutTrees,
+                landBurn: req.body.landBurn,
+                highway: req.body.highway,
+                industrial: req.body.industrial,
+                publicLand: req.body.publicLand,
+                widened: req.body.widened,
+                inhabitedPrivate: req.body.inhabitedPrivate,
+                uninhabitedPrivate: req.body.uninhabitedPrivate,
+                centerProperty: req.body.centerProperty,
+                perimeterProperty: req.body.perimeterProperty,
+                forest: req.body.forest,
+            },
+            
+            coordinates: new admin.firestore.GeoPoint(lat, long)
+        }).then((ref)=>{
+            return console.log(ref)
+        }).catch((err)=>{
+            return console.log(err)
+        })
 
         res.status(200).json({
-            message: 'Data sent'
+            message: 'Data Sent'
         })
     }else{
-
         res.status(500).json({
-            message: 'Invalid Request'
+            message:'Invalid Request'
         })
-
     }
-
- })
  
 })
 
-//POST method to get querying parameters.
-// Returns all markers within radius
+})
+
+
 exports.getData = functions.https.onRequest((req, res)=>{
 
     cors(req, res, ()=>{
 
-        var userLat = Number(req.body.userLat);
-        var userLong = Number(req.body.userLong);
-        const center = geo.point(userLat, userLong);
-        const radius = Number(req.body.radius);
-        const field = 'position'
-    
-        const query = geo.collection('trees').within(center, radius, field);  //query is an Observable
+        if(req.method === 'POST'){
 
-        query.subscribe(data=>{
-            res.send(data)},
-            (err)=>{
-                res.send('error:'+ err)
-            },
-            (results)=>{
-                res.send(results)
-            }
-        )
+            var radius = Number(req.body.radius)
+            var userLat = Number(req.body.userLat)
+            var userLong = Number(req.body.userLong)
 
-}
-)
-}
-)
+            var query = geoCollection.near({ center: new admin.firestore.GeoPoint(userLat, userLong), radius: radius });
+
+            query.get().then((snap)=>{
+                return res.send(snap.docs)
+            }).catch((err)=>{
+                throw res.send(err)
+            })
+
+        }else{
+            res.status(500).json({
+                message: 'Invalid Request'
+            })
+        }
+    })
+})
