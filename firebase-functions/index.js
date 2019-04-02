8027829994cf2d2b681806b310d1666d09eb6634
Dave Paiva <34977572+davepaiva@users.noreply.github.com>
2019-04-02 20:07:32 +0530

    Update index.js

--- firebase-functions/index.js
+++ firebase-functions/index.js
@@ -18,82 +18,76 @@ const  geoFirestore = new geofirestore.GeoFirestore(defaultFirestore);
 const geoCollection = geoFirestore.collection('trees');
 
 
-
-
-
-
-
 exports.postData = functions.https.onRequest((req, res)=>{
- cors(req, res, ()=>{
-
-
-
-
-    if(req.method === 'POST'){
-
-        var lat = Number(req.body.lat);
-        var long = Number(req.body.long);
-    
-        geoCollection.add( { 
-            
-            info : {
-                genericName: req.body.generic_name,
-                scientificName: req.body.scientific_name,
-                type: req.body.type,
-                leafStructure: req.body.leaf_structure,
-                rootStructure: req.body.root_structure,
-                age: req.body.age,
-                flower: req.body.flower,
-                fruit: req.body.fruit
-                    },
-            healthChecks: {
-                cutBranches: req.body.cut_branches,
-                sap: req.body.sap,
-                branchCrack: req.body.cracked_branches,
-                brownmud: req.body.brownmud,
-                tumours: req.body.tumours,
-                fungus: req.body.fungus,
-                wilting: req.body.wilting,
-                saprophyte: req.body.saprophyte,
-                fire: req.body.fire,
-                stripped: req.body.stripped,
-                construction: req.body.construction,
-                branchesCut: req.body.branchesCut,
-             },
-            environmentalRisks: {
-                overgrownBranches: req.body.overgrownBranches,
-                cutTrees: req.body.cutTrees,
-                landBurn: req.body.landBurn,
-                highway: req.body.highway,
-                industrial: req.body.industrial,
-                publicLand: req.body.publicLand,
-                widened: req.body.widened,
-                inhabitedPrivate: req.body.inhabitedPrivate,
-                uninhabitedPrivate: req.body.uninhabitedPrivate,
-                centerProperty: req.body.centerProperty,
-                perimeterProperty: req.body.perimeterProperty,
-                forest: req.body.forest,
-            },
-            
-            coordinates: new admin.firestore.GeoPoint(lat, long)
-        }).then((ref)=>{
-            return console.log(ref)
-        }).catch((err)=>{
-            return console.log(err)
-        })
-
-        res.status(200).json({
-            message: 'Data Sent'
-        })
-    }else{
-        res.status(500).json({
-            message:'Invalid Request'
-        })
-    }
+  cors(req, res, ()=>{
  
-})
+ 
+ 
+ 
+     if(req.method === 'POST'){
+ 
+         var lat = Number(req.body.lat);
+         var long = Number(req.body.long);
+     
+         geoCollection.add( { 
+            info: {
+             genericName: req.body.generic_name,
+             scientificName: req.body.scientific_name,
+             type: req.body.type,
+             leafStructure: req.body.leaf_structure,
+             rootStructure: req.body.root_structure,
+             age: req.body.age,
+             flower: req.body.flower,
+             fruit: req.body.fruit,
+            },
+            healthChecks: {
+             cutBranches: req.body.cut_branches,  //Do you see broken or cut branches.
+             sap: req.body.sap,                    //Do you see sap oozing out from the tree trunk.
+             branchCrack: req.body.cracked_branches,   //Do you see holes or cracks in the branches or tree.
+             brownmud: req.body.brownmud, //Brown mud like deposit on the tree or the trunk.
+             tumours: req.body.tumours, //Tumors, bulges or swellings noticed on the trunk or branches.
+             fungus: req.body.fungus,  //Fungus visible on the branches or on the trunk.
+             wilting: req.body.wilting,  //Curling wilting or dis-colourisation notice in the leaves.
+             saprophyte: req.body.saprophyte,  //Saprophyte or epiphyte growing on the tree.
+             fire: req.body.fire,  //Signs of fire being burnt near the tree. (anywhere under the canopy of the tree)
+             stripped: req.body.stripped,  //Outermost layer of the tree trunk stripped.
+             construction: req.body.construction, //Perimeter or construction built around the tree.
+             branchesCut: req.body.branchesCut,  //Branches cut for electrical wires
+            },
+            environmentalRisks: {
+             overgrownBranches: req.body.overgrownBranches,  //Overgrown branches close to electric/telephone wires.
+             cutTrees: req.body.cutTrees, //Signs of other trees being cut down in the area.
+             landBurn: req.body.landBurn, //Land being cleared by burning.
+             highway: req.body.highway,  //Tree on a highway stretch.
+             industrial: req.body.industrial,  //Trees located near industrial lands.
+             publicLand: req.body.publicLand, //Tree on public land.
+             widened: req.body.widened, //Tree on an inner road likely to be widened.
+             habitatedPrivate: req.body.habitatedPrivate,  //Tree in an inhabited private property.
+             unhabitatedPrivate: req.body.unhabitatedPrivate,  //Tree in an habited private property.
+             centerProperty: req.body.centerProperty,  //Tree located in the center of the property
+             perimeterProperty: req.body.perimeterProperty, //Tree located at the perimeter of the property.
+             forest: req.body.forest,  //Tree in a forest.
+            },
+             coordinates: new admin.firestore.GeoPoint(lat, long)
+            }).then((docRef)=>{
+                return console.log(docRef)
+            }).catch((err)=>{
+                throw console.log(err)
+            })
+ 
+     }else{
+         res.status(500).json({
+             message:'Invalid Request'
+         })
+     }
+  })
 
 })
+
+
+
+
+
 
 
 exports.getData = functions.https.onRequest((req, res)=>{
