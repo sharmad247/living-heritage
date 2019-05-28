5494cfd5189e03f24d5dc4c9df20ad8c8264fdcb
sharmad247 <sharmad247@gmail.com>
2019-05-28 13:06:44 +0530

    Username and signout

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,10 +11,8 @@ import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+
 
 import logo from '../Assets/logo.png'
 
@@ -58,17 +56,16 @@ class Header extends Component {
             </List>
             <Divider />
             <List>
-              {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                  <ListItemText primary={text} />
+                <ListItem>
+                  <ListItemText primary={this.props.user.displayName} />
                 </ListItem>
-              ))}
+                <ListItem button onClick={this.props.signout}>
+                  <ListItemText primary="Sign Out" />
+                </ListItem>
             </List>
           </div>
         );
     
-
         return (
             <div className={this.state.classes.root}>
               <AppBar position="fixed">
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,19 +5,17 @@ import {Route} from 'react-router-dom'
 import Add from './Add'
 import Map from '../Components/Map'
 import TreeInfo from './TreeInfo'
-import SignIn from './SignIn';
 
 export default function Home(props) {
   return (
     <div>
-      {props.user ? <Header /> : null}
+      {props.user ? <Header user={props.user} signout={props.signOut}/> : null}
       <div className="ContainerStyle">
         <Route exact path="/" component={Map}/>
         <Route path="/addtree" component={Add}/>
         <Route path="/treeinfo" component={TreeInfo}/>
       </div>
       <Route exact path="/" render={() => <Navbar action={props.action} value={0} />} />
-      <Route path="/signin" component={SignIn}/>
     </div>
   )
 }
