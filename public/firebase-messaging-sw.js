a8c795e1176651828e8cf5ad770c30b12f06d99c
sharmad247 <sharmad247@gmail.com>
2019-07-16 18:21:15 +0530

    Beta Test FCM

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-messaging.js');
+firebase.initializeApp({
+  'messagingSenderId': '773233255134'
+});
+const messaging = firebase.messaging();
