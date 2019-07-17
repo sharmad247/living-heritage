dd16e6bc8416a042d18be03fef095981709bec9d
sharmad247 <sharmad247@gmail.com>
2019-07-17 12:17:38 +0530

    images fix

--- src/App.js
+++ src/App.js
@@ -24,15 +24,12 @@ ReactGA.pageview(window.location.pathname);
 
 let firebaseApp = firebase.initializeApp(Config)
 let firebaseAppAuth = firebaseApp.auth()
-<<<<<<< HEAD
+let messaging
 if (firebase.messaging.isSupported()) {
-  let messaging = firebase.messaging()
+  messaging = firebase.messaging()
   messaging.usePublicVapidKey("BP1bB1VIB1zziviY38GM1DoEiwm-gVGXtl2yqbjGYzrhuxMRdMhv1hh8CBVnrt_ggFPBhX87Cr4NY8IzYAKvX50")
 }
-=======
-let messaging = firebase.messaging()
-messaging.usePublicVapidKey("BP1bB1VIB1zziviY38GM1DoEiwm-gVGXtl2yqbjGYzrhuxMRdMhv1hh8CBVnrt_ggFPBhX87Cr4NY8IzYAKvX50")
->>>>>>> 100fa5ad9d815d8968200b80e8896c4ae4cfce38
+
 //let messaging = firebase.messaging()
 let providers = {
   googleProvider: new firebase.auth.GoogleAuthProvider(),
--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -191,12 +191,12 @@ class SimpleExpansionPanel extends Component {
     let db = this.props.location.firebaseApp.firestore()
 
     //Saves tree UID and coordinates in the index
-    db.collection('tree_index').doc('mapload').update({
+    db.collection('trees_index').doc('mapload').update({
       data: firebase.firestore.FieldValue.arrayUnion({ "id": uid, "pos": { "lat": pos.lat, "long": pos.lng }, "img": this.state.downloadURLs[0] })
     });
 
     //saves main info on tree
-    db.collection('tree_data').doc(uid).set({
+    db.collection('trees').doc(uid).set({
       info: {
         genericName: this.state.genericName,
         scientificName: this.state.scientificName, //string,
